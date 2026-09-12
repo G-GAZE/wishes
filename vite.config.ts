9fbf4048038218c91ec1f4e238215df77b4e0449
GeZhe-GAZE <g_gaze715@qq.com>
2026-09-12 19:57:33 +0800

    build(frontend): 引入 NaiveUI 组件库

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import Components from 'unplugin-vue-components/vite';
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 import path from "path";
 
 import pkg from "./package.json";
@@ -9,7 +11,12 @@ const host = process.env.TAURI_DEV_HOST;
 
 // https://vite.dev/config/
 export default defineConfig(async () => ({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [NaiveUiResolver()]
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
